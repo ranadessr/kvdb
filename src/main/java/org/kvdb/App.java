64dcdc188dcd61a4d84d50566b8ad168033fcd5f
Sandeep Ranade <ranadessr@gmail.com>
2024-09-08 19:49:26 -0400

    configure port via -D option

--- src/main/java/org/kvdb/App.java
+++ src/main/java/org/kvdb/App.java
@@ -13,28 +13,12 @@ public class App
     public static void main( String[] args ) {
         // obtain an instance of the db
         SessionManager sessMgr = new SessionManager();
-/*        Session sess = sessMgr.createSession();
-        Session ses2 = sessMgr.createSession();
+        int port = 8080;
+        if (System.getProperty("port") != null) {
+            port = Integer.parseInt(System.getProperty("port"));
+        }
 
-        // opertations can be performed outside a transaction and are immediately
-        // applied to the backing store
-        sess.put("key1", "somevalue");
-        sess.startTransaction();
-        sess.put("foo", "bar");
-        sess.put("baz", "bat");
-        sess.delete("baz");
-        System.out.println(sess.get("key1"));
-        sess.commitTransaction();
-        System.out.println(sess.get("baz"));
-        System.out.println(sess.get("foo"));
-
-        // this transaction will not commit because of a conflicting write
-        sess.startTransaction();
-        sess.put("k1", "v1");
-        ses2.put("k1", "v2");
-        sess.commitTransaction();
-        System.out.println(sess.get("k1"));*/
-        KvDbServer server = new KvDbServer(sessMgr, 8080);
+        KvDbServer server = new KvDbServer(sessMgr, port);
         try {
             server.run();
         } catch (Exception e) {
